d819b6c3dbdfb937517a4ff669b0fdb98da867ef
Francis Bouvier <francis.bouvier@gmail.com>
2023-07-12 15:40:17 +0200

    Node.DOCUMEN_POSITION
    Signed-off-by: Francis Bouvier <francis.bouvier@gmail.com>

--- src/dom/node.zig
+++ src/dom/node.zig
@@ -44,6 +44,16 @@ pub const Node = struct {
         };
     }
 
+    // JS static
+    // ---------
+
+    pub const _DOCUMENT_POSITION_DISCONNECTED = 1;
+    pub const _DOCUMENT_POSITION_PRECEDING = 2;
+    pub const _DOCUMENT_POSITION_FOLLOWING = 4;
+    pub const _DOCUMENT_POSITION_CONTAINS = 8;
+    pub const _DOCUMENT_POSITION_CONTAINED_BY = 16;
+    pub const _DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC = 32;
+
     // JS funcs
     // --------
 
